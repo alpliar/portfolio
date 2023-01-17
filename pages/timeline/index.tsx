607535fe177058bff5ede5dad6c6596d1145a2cc
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-01-17 15:36:55 +0100

    feature: timeline page

---
+++ pages/timeline/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Timeline from "../../components/Timeline";
+import PageLayout from "../../layouts/PageLayout";
+
+type Props = {};
+
+const TimelinePage = () => {
+  return (
+    <PageLayout>
+      <Timeline />
+    </PageLayout>
+  );
+};
+
+export default TimelinePage;

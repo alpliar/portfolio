9874ad633356c6e5b308f2511262db0425b0d12b
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-01-17 15:38:44 +0100

    chore: update timeline data

--- data/timeline.data.tsx
+++ data/timeline.data.tsx
@@ -1,49 +1,52 @@
-import { HiBadgeCheck, HiBookmark } from "react-icons/hi";
-import { MdRunCircle, MdSchool } from "react-icons/md";
+import { HiBookmark } from "react-icons/hi";
+import { MdDownloading, MdSchool } from "react-icons/md";
 import { VscDebugBreakpointLog } from "react-icons/vsc";
 import { Timeline } from "../models/TimelineData.model";
 
 const jobIcon = HiBookmark;
 const missionIcon = VscDebugBreakpointLog;
 const schoolIcon = MdSchool;
-const searchingIcon = MdRunCircle;
+const searchingIcon = MdDownloading;
 
-const recentEventColor = "green.300";
-const jobColor = "orange.300";
-const studyColor = "purple.300";
+const recentEventColor = "orange.800";
+const extiaColors = "orange.700";
+const hrTeamColors = "orange.600";
+const gfiColors = "orange.500";
+const natSysColors = "orange.400";
+const studyColor = "orange.300";
 
 const timelineData: Timeline = [
   {
     logo: searchingIcon,
     color: recentEventColor,
     date: "Those days",
-    title: "Looking for a frontend developer position",
+    title: "Looking for opportunities",
     description: "Lyon, France",
   },
   {
     logo: jobIcon,
-    color: jobColor,
+    color: extiaColors,
     date: "2020 to 2021",
     title: "Extia",
     description: "1 year 1 month",
     steps: [
       {
         logo: missionIcon,
-        color: jobColor,
+        color: extiaColors,
         date: "2021",
         title: "Bonduelle Group",
         description: "Frontend Developer",
       },
       {
         logo: missionIcon,
-        color: jobColor,
+        color: extiaColors,
         date: "2021",
         title: "Extia",
         description: "Frontend Developer",
       },
       {
         logo: missionIcon,
-        color: jobColor,
+        color: extiaColors,
         date: "2020",
         title: "Camaïeu",
         description: "Frontend Developer",
@@ -52,21 +55,21 @@ const timelineData: Timeline = [
   },
   {
     logo: jobIcon,
-    color: jobColor,
+    color: hrTeamColors,
     date: "2018 to 2020",
     title: "HR Team",
     description: "3 years",
     steps: [
       {
         logo: missionIcon,
-        color: jobColor,
+        color: hrTeamColors,
         date: "2018 to 2020",
         title: "La Redoute",
         description: "Technical project manager",
       },
       {
         logo: missionIcon,
-        color: jobColor,
+        color: hrTeamColors,
         date: "2020",
         title: "Leroy Merlin / Adeo",
         description: "Software engineer",
@@ -75,21 +78,21 @@ const timelineData: Timeline = [
   },
   {
     logo: jobIcon,
-    color: jobColor,
+    color: gfiColors,
     date: "2010 to 2018",
     title: "Gfi world",
     description: "8 years 1 month",
     steps: [
       {
         logo: missionIcon,
-        color: jobColor,
+        color: gfiColors,
         date: "2014 to 2017",
         title: "Decathlon",
         description: "Technical support engineer",
       },
       {
         logo: missionIcon,
-        color: jobColor,
+        color: gfiColors,
         date: "2010 to 2014",
         title: "BPCE Group",
         description: "Software engineer",
@@ -98,7 +101,7 @@ const timelineData: Timeline = [
   },
   {
     logo: jobIcon,
-    color: jobColor,
+    color: natSysColors,
     date: "2008 to 2009",
     title: "Nat System",
     description: "1 year 6 months",

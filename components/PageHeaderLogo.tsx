f0a277dea35c3d542af3bd42522f299724aa8e49
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-01-27 00:49:38 +0100

    fix: prevent logo blinking on page start

--- components/PageHeaderLogo.tsx
+++ components/PageHeaderLogo.tsx
@@ -1,13 +1,20 @@
 import { Avatar, Heading, HStack } from "@chakra-ui/react";
+import Image from "next/image";
 import React from "react";
+import avatar from "../assets/avatar.webp";
 
 type Props = {};
 
 const PageHeaderLogo: React.FC<Props> = ({}) => {
-  // return <Heading>Portfolio</Heading>;
   return (
     <HStack>
-      <Avatar src="/avatar.webp" />
+      <Image
+        src={avatar.src}
+        alt="avatar of the author"
+        width={avatar.width / 4}
+        height={avatar.height / 4}
+      />
+      {/* <Avatar src={avatar.src} /> */}
       <Heading as="p">Portfolio</Heading>
     </HStack>
   );
